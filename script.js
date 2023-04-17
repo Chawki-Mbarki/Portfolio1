7baf2e65c8f9e9f24974abfe16db34c70784d2c3
Chawki <chawkimbarki500@gmail.com>
2023-04-17 10:28:51 +0100

    Fix all linter errors and issues

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// the displayNavigation function will toggle the Display-nav class showing/hiding the nav element
 function displayNavigation() {
-    const nav = document.querySelector('nav');
-    nav.classList.toggle('displayNav');
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('display-nav');
 }
 
 const menu = document.querySelector('.menu');
+const xmark = document.querySelector('.xmark');
+const navLinks = document.querySelectorAll('.nav-link');
 menu.addEventListener('click', displayNavigation);
+xmark.addEventListener('click', displayNavigation);
+
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', displayNavigation);
+}
